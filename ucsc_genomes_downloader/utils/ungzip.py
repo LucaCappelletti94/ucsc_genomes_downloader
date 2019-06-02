46a8c21298f7e06b652d01c75a4970d8d7202d6a
Luca Cappelletti <cappelletti.luca94@gmail.com>
2019-06-02 21:59:38 +0200

    Resolved code smell

--- ucsc_genomes_downloader/utils/ungzip.py
+++ ucsc_genomes_downloader/utils/ungzip.py
@@ -5,7 +5,6 @@ def ungzip(path:str):
     """Ungzip file at given path.
         path:str, path of the file to extract.
     """
-    assert path.endswith(".gz")
     with gzip.open(path, 'rb') as f_in:
         with open(path[:-3], 'wb') as f_out:
             shutil.copyfileobj(f_in, f_out)
