49994d6f80e6ba66e8281483237052b9b887b123
Luca Cappelletti <cappelletti.luca94@gmail.com>
2019-12-15 13:12:04 +0100

    Extended testing

--- tests/test_genomes_downloader.py
+++ tests/test_genomes_downloader.py
@@ -14,6 +14,34 @@ def test_genomes_downloader():
     os.remove("hg19.fa")
 
 
+def test_multiple_downloads():
+    genomes = [
+        "hg38",
+        "hg19",
+        "hg18",
+        "hg17",
+        "hg16",
+        "hg15",
+        "hg13",
+        "hg12",
+        "hg11",
+        "hg10",
+        "mm10",
+        "mm9",
+        "mm8",
+        "mm7",
+        "mm6",
+        "mm5",
+        "mm4",
+        "mm3",
+        "mm2",
+        "mm1"
+    ]
+    for genome in genomes:
+        download_genome(genome, clear_cache=True, chromosomes=["chrM"])
+        os.remove(f"{genome}.fa")
+
+
 def test_invalid_chromosome():
     with pytest.raises(ValueError):
         download_genome("hg19", clear_cache=True, chromosomes=["chrU"])
@@ -23,9 +51,3 @@ def test_full_download():
     download_genome("hg19", clear_cache=True)
     assert os.path.exists("hg19.fa")
     os.remove("hg19.fa")
-
-
-def test_full_old_download():
-    download_genome("hg4", clear_cache=True)
-    assert os.path.exists("hg4.fa")
-    os.remove("hg4.fa")
