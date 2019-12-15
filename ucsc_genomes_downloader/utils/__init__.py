565ec74bd3b3f0aae3fa985b805d2baa0cf735ed
Luca Cappelletti <cappelletti.luca94@gmail.com>
2019-12-15 12:55:45 +0100

    Removed dead code

--- ucsc_genomes_downloader/utils/__init__.py
+++ ucsc_genomes_downloader/utils/__init__.py
@@ -1,6 +1,5 @@
 from .download import download
 from .download_tasks import download_tasks
 from .merge_genome import merge_genome
-from .all_chromosomes import all_chromosomes
 
-__all__ = ["download", "download_tasks", "merge_genome", "all_chromosomes"]
+__all__ = ["download", "download_tasks", "merge_genome"]
