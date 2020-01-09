850a47053c722944e6879f0aba2d00862059d4e1
Luca Cappelletti <cappelletti.luca94@gmail.com>
2020-01-09 11:32:45 +0100

    Now conserving informations in bed filNow conserving informations in bed filee

--- ucsc_genomes_downloader/utils/extract_sequences.py
+++ ucsc_genomes_downloader/utils/extract_sequences.py
@@ -5,12 +5,10 @@ def multiprocessing_extract_sequences(kwargs):
     return extract_sequences(**kwargs)
 
 
-def extract_sequences(bed: pd.DataFrame, sequence: str):
+def extract_sequences(bed: pd.DataFrame, sequence: str)->pd.DataFrame:
     return pd.DataFrame([
         {
-            "chrom": row.chrom,
-            "chromStart": row.chromStart,
-            "chromEnd": row.chromEnd,
+            **row.to_dict(),
             "sequence": sequence[row.chromStart:row.chromEnd]
         }
         for _, row in bed.iterrows()
